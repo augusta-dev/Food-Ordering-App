401f2a4b5f93e702b6cdcf918aa13e66c7670993
augustan-dev <augustan.dev@gmail.com>
2023-08-03 21:57:54 +0300

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
-const {nextui} = require("@nextui-org/react");
+const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    // ...
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
-  plugins: [nextui()],
+	content: [
+		// ...
+		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				cormorant: "Cormorant Garamond",
+				rasa: "Rasa",
+				sofia: "Princess Sofia",
+			},
+		},
+	},
+	darkMode: "class",
+	plugins: [nextui()],
 };
-
