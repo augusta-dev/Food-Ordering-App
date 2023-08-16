173b62e81f13b0ed6c438c2e054dce295af27a16
augustan-dev <augustan.dev@gmail.com>
2023-08-16 23:06:32 +0300

    Done

--- src/Components/Store/CartProvider.js
+++ src/Components/Store/CartProvider.js
@@ -62,7 +62,7 @@ const cartReducer = (state, action) => {
 			(item) => item.id === action.id,
 		);
 		const existingItem = state.items[existingCartItemIndex];
-		const updatedTotalAmount = state.totalAmout -+(existingItem.price);
+		const updatedTotalAmount = state.totalAmount -+(existingItem.price);
 		let updatedItems;
 		if (existingItem.amount === 1) {
 			updatedItems = state.items.filter((item) => item.id !== action.id);
