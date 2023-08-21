814d39d0793117e60b11a3de39a6a25e4f25c309
augustan-dev <augustan.dev@gmail.com>
2023-08-21 23:11:01 +0300

    Update to link

--- src/Components/Store/CartProvider.js
+++ src/Components/Store/CartProvider.js
@@ -33,7 +33,6 @@ const cartReducer = (state, action) => {
 		} else {
 			updatedItems = state.items.concat(action.item);
 		}
-		console.log(updatedItems);
 		return {
 			items: updatedItems,
 			totalAmount: updatedTotalAmount,
